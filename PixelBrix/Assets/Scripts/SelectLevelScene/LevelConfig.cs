55e231cd0233cf93d532d71b9048311ecc852ede
omeralpcolak <omer.alp.colak@gmail.com>
2024-05-28 13:43:32 +0300

    feat(sprite): Adding enemys' other states

--- PixelBrix/Assets/Scripts/SelectLevelScene/LevelConfig.cs
+++ PixelBrix/Assets/Scripts/SelectLevelScene/LevelConfig.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class LevelConfig : ScriptableObject
 {
     public string text;
-    public Sprite sprite;
-    public Sprite selectedSprite;
+    public Sprite idleSprite;
+    public Sprite angrySprite;
+    public Sprite evilSprite;
     public bool isLevelCompleted
     {
         get => PlayerPrefs.GetInt("IsLevelCompleted", 0) == 1;
         set => PlayerPrefs.SetInt("IsLevelCompleted", value ? 1 : 0);
     }
+    public bool isItFirstLevel;
 }
