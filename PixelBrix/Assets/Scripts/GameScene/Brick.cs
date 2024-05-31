44b9b38281700aa1fe4884f254acacdc667f4513
omeralpcolak <omer.alp.colak@gmail.com>
2024-06-01 00:46:35 +0300

    feat(script): Brick.cs
    #Example script is written for controlling brick game objects.

---
+++ PixelBrix/Assets/Scripts/GameScene/Brick.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Brick : MonoBehaviour
+{
+    [HideInInspector]public WaveController owner;
+    public Vector3 finalPos;
+
+    public void Init(WaveController _owner)
+    {
+        gameObject.SetActive(false);
+        owner = _owner;
+        owner.hp += 1;
+        transform.parent = null;
+        finalPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        transform.parent = owner.transform;
+        owner.OnWaveAnim += Move;
+        
+
+    }
+
+
+    public void Move()
+    {
+        gameObject.SetActive(true);
+        transform.parent = null;
+        transform.DOMoveY(finalPos.y, 1f)
+            .SetEase(Ease.InFlash);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            owner.hp -= 1;
+            //InsantiateEffect;
+            FindAnyObjectByType<LevelManager>().CheckEligiableForNextLevel(owner.hp);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        owner.OnWaveAnim -= Move;
+    }
+
+
+
+}
