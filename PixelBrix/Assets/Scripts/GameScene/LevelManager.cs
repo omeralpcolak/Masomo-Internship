4ee7fe52325f353ace4af961e39b81869d53fe1e
omeralpcolak <omer.alp.colak@gmail.com>
2024-06-01 00:50:42 +0300

    feat(script): LevelManager
    #Level manager will be used for maintaining the each brick wave.

---
+++ PixelBrix/Assets/Scripts/GameScene/LevelManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using DG.Tweening;
+
+[System.Serializable]
+public class Wave
+{
+    public WaveController wavePrefab;
+    public void CreateTheWave(Transform transform)
+    {
+        WaveController wave = GameObject.Instantiate(wavePrefab, transform.position, Quaternion.identity);
+        wave.SetUpWave();
+        wave.WaveAnim();
+    }
+}
+
+
+public class LevelManager : MonoBehaviour
+{
+    private int index;
+    public List<Wave> waves;
+    public Wave CurrentWave => waves[index];
+
+    private void Start()
+    {
+        index = 0;
+        InsTheWave();
+    }
+
+    public void InsTheWave()
+    {
+        CurrentWave.CreateTheWave(transform);
+    }
+
+    public void CheckEligiableForNextLevel(int hp)
+    {
+        if(hp <= 0)
+        {
+            NextWave();
+        }
+    }
+
+    public void NextWave()
+    {
+        index++;
+        if(index < waves.Count)
+        {
+            InsTheWave();
+        }
+        else
+        {
+            //Player wins
+            //Enemy defated.
+        }
+    }
+}
